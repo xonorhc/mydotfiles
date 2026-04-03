51a465a4531b245b03d9e7c8e242d4b96f840b39
xonorhc <xonorhc@proton.me>
2026-04-02 22:13:54 -0400

    chore(colors): update colorscheme to cyan

--- dotfiles/.config/matugen/templates/nvim-base16.lua
+++ dotfiles/.config/matugen/templates/nvim-base16.lua
@@ -5,23 +5,10 @@ return {
 	config = function()
 		require("base16-pro-max").setup({
 			colors = {
-				base00 = "{{colors.background.default.hex}}",
-				base01 = "{{colors.surface_container_lowest.default.hex}}",
-				base02 = "{{colors.surface_container_highest.default.hex}}",
-				base03 = "{{colors.outline_variant.default.hex}}",
-				base04 = "{{colors.on_surface_variant.default.hex}}",
-				base05 = "{{colors.on_surface.default.hex}}",
-				base06 = "{{colors.inverse_on_surface.default.hex}}",
-				base07 = "{{colors.surface_bright.default.hex}}",
-				base08 = "{{colors.error.default.hex}}",
-				base09 = "{{colors.tertiary.default.hex}}",
-				base0A = "{{colors.secondary.default.hex}}",
-				base0B = "{{colors.primary.default.hex}}",
-				base0C = "{{colors.tertiary_fixed.default.hex}}",
-				base0D = "{{colors.primary_fixed.default.hex}}",
-				base0E = "{{colors.secondary_fixed.default.hex}}",
-				base0F = "{{colors.error_container.default.hex}}",
-			},
+        <* for name, value in base16 *>
+        {{name | camel_case}} = "{{ value.default.hex | harmonize: {{ colors.source_color.default.hex | to_color }} | to_color | lighten: 20 | auto_lightness: 10 | to_color | saturate: 35, "hsl" }}",
+        <* endfor *>
+      },
 			styles = { italic = true, transparency = true },
 			plugins = { enable_all = true },
 		})
--- dotfiles/.config/nvim/colors/matugen.vim
+++ dotfiles/.config/nvim/colors/matugen.vim
@@ -1,20 +1,20 @@
-hi Comment guibg=None guifg=#3b6065
+hi Comment guibg=None guifg=#244d49
 
-hi Delimiter guibg=None guifg=#5f8b90
-hi Operator guibg=None guifg=#5f8b90
+hi Delimiter guibg=None guifg=#3a706b
+hi Operator guibg=None guifg=#3a706b
 
-hi Todo guibg=None guifg=#71a0a6
+hi Todo guibg=None guifg=#44817b
 
-hi Identifier guibg=None guifg=#ff85c6
-hi Constant guibg=None guifg=#e097ca
-hi Type guibg=None guifg=#5bbbcd
-hi String guibg=None guifg=#62c41e
-hi Special guibg=None guifg=#97acef
-hi PreProc guibg=None guifg=#97acef
-hi Function guibg=None guifg=#00bfd7
-hi Statement guibg=None guifg=#90abff
+hi Identifier guibg=None guifg=#f68500
+hi Constant guibg=None guifg=#d69549
+hi Type guibg=None guifg=#4eb2a5
+hi String guibg=None guifg=#72b400
+hi Special guibg=None guifg=#6ba8da
+hi PreProc guibg=None guifg=#6ba8da
+hi Function guibg=None guifg=#00b6a5
+hi Statement guibg=None guifg=#00abfe
 
-hi Error guibg=#dd342e guifg=#ffffff
-hi StatusLine guibg=#00dbf6 guifg=#00282e
-hi StatusLineNC guibg=#008293 guifg=#ffffff
-hi Selection guibg=#2a4b4f
+hi Error guibg=#ff574c guifg=#000000
+hi StatusLine guibg=#80f2e2 guifg=#003d36
+hi StatusLineNC guibg=#23ac9c guifg=#000000
+hi Selection guibg=#1a3c39
