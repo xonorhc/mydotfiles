2b9a9cb38b7e053c57e5d1e51faddcac5cc831f2
heron.golin <heron.golin@globalmtiete.com.br>
2026-06-01 08:17:09 -0300

    update yazi

--- dotfiles/.config/yazi/init.lua
+++ dotfiles/.config/yazi/init.lua
@@ -1,13 +1,1 @@
-require("git"):setup({
-	order = 1500,
-})
-
-require("session"):setup({
-	sync_yanked = true,
-})
-
-require("what-size"):setup({
-	priority = 400,
-	LEFT = "",
-	RIGHT = " ",
-})
+require("no-status"):setup()
